51e9bfbc9f51e2f119918efb2b48d03ead096f63
RayFangRF <rayfang990106@gmail.com>
2019-03-09 01:04:35 -0500

    load file function added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+typedef unsigned int inti;
 using namespace std;
-
 int main() {
     cout << "Which file(s) to open?\n";
     cout << "1. friendlyships.shp" << endl;
@@ -19,4 +21,76 @@ int main() {
     cout << "5. Unarmed ships" << endl;
 
     cin >> option;
+}
+struct Arsenal{
+    string weaponName;
+    int weaponNumber;
+    float weaponPower;
+};
+struct Battleship{
+    string shipName;
+    string shipClass;
+    short shipLength;
+    int shieldCapacity;
+    float maxWarpSpeed;
+    vector<Arsenal> inventory;
+};
+void loadFile(vector<Battleship>& ships, int fileNum){
+    string path;
+    if(fileNum == 1){
+        path = "friendlyships.shp";
+    }else if(fileNum == 2){
+        path = "enemyships.shp";
+    }else{
+        cout<<"incorrect input!"<<endl;
+    }
+    ifstream file(path, ios_base::binary);
+    if(file.is_open()){
+        inti count;
+        file.read((char*) &count, 4);
+        for(inti i = 0; i < count; i++){
+            Battleship newShip;
+            inti nameLength;
+            inti classLength;
+            inti inventorySize = 0;
+            file.read((char*)&nameLength, 4);
+            file.read((char*)&classLength, 4);
+            char* shipName = new char[nameLength];
+            char* className = new char[classLength];
+            for(inti j = 0; j< nameLength; j++){
+                file.read(&shipName[j], 1);
+            }
+            for(inti z = 0; z< classLength; z++){
+                file.read(&className[z], 1);
+            }
+            newShip.shipName = shipName;
+            newShip.shipClass = className;
+            delete[] shipName;
+            delete[] className;
+            file.read((char*) &newShip.shipLength,2);
+            file.read((char*) &newShip.shieldCapacity, 4);
+            file.read((char*) &newShip.maxWarpSpeed, 4);
+            file.read((char*) &inventorySize, 4);
+            //load in inventory
+            if(inventorySize > 0){
+                Arsenal ars;
+                for(inti u = 0; u < inventorySize; u++){
+                    file.read((char*) &nameLength,4);
+                    char* name = new char[nameLength];
+                    for(inti a = 0; a >nameLength; a++){
+                        file.read(&name[a],1);
+                    }
+                    ars.weaponName = name;
+                    delete[] name;
+                    file.read((char*)&ars.weaponNumber, 4);
+                    file.read((char*)&ars.weaponPower, 4);
+                    newShip.inventory.push_back(ars);
+                }
+
+            }
+            ships.push_back(newShip);
+
+        }
+        file.close();
+    }
 }
